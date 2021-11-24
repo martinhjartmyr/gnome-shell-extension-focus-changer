84a20e19229204ebace3163c7a926c49b8aaedd1
Martin Hjärtmyr <martin@hjartmyr.se>
2021-11-24 22:34:18 +0100

    Initial commit

---
+++ extension.js
@@ -0,0 +1,273 @@
+'use strict';
+
+const { Shell, Meta } = imports.gi;
+const Main = imports.ui.main;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+const SCHEMA_FOCUS_UP = 'focus-up';
+const SCHEMA_FOCUS_DOWN = 'focus-down';
+const SCHEMA_FOCUS_RIGHT = 'focus-right';
+const SCHEMA_FOCUS_LEFT = 'focus-left';
+
+class FocusChanger {
+    constructor() {
+        this._settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.focus-changer');
+        this._workspaceManager = global.workspace_manager;
+        this._keyFocusUpId = null;
+        this._keyFocusDownId = null;
+        this._keyFocusRightId = null;
+        this._keyFocusLeftId = null;
+        this._activeWindow = null;
+
+        this._bindShortcut();
+    }
+
+    changeFocus(id) {
+        // log('focus-changer', id);
+        const { activeWindow, activeRect } = this._getActiveWindow();
+        if (!activeWindow) {
+            this._activeWindow = null;
+            return;
+        }
+
+        this._activeWindow = activeWindow;
+        const bestCandidate = this._getBestCandidate(id, activeWindow.get_monitor(), activeRect);
+        if (bestCandidate)
+            bestCandidate.activate(global.get_current_time());
+    }
+
+    _getMonitorByDirection(id, activeMonitorId) {
+        const numberOfMonitors = this._activeWindow.get_display().get_n_monitors();
+
+        let activeMonitor = null;
+        const monitors = [];
+        for (let i = 0; i < numberOfMonitors; i++) {
+            if (i === activeMonitorId)
+                activeMonitor = this._activeWindow.get_display().get_monitor_geometry(i);
+            else
+                monitors.push({ id: i, rect: this._activeWindow.get_display().get_monitor_geometry(i) });
+        }
+
+        if (!activeMonitor || !monitors.length)
+            return null;
+
+
+        let bestMonitorCandidate = null;
+        switch (id) {
+        case SCHEMA_FOCUS_UP:
+            for (let m of monitors) {
+                if (m.rect.y < activeMonitor.y) {
+                    if (!bestMonitorCandidate)
+                        bestMonitorCandidate = m;
+                    else if (Math.abs(activeMonitor.x - m.rect.x) < Math.abs(activeMonitor.x - bestMonitorCandidate.rect.x))
+                        bestMonitorCandidate = m;
+                }
+            }
+            break;
+        case SCHEMA_FOCUS_DOWN:
+            for (let m of monitors) {
+                if (m.rect.y > activeMonitor.y) {
+                    if (!bestMonitorCandidate)
+                        bestMonitorCandidate = m;
+                    else if (Math.abs(activeMonitor.x - m.rect.x) < Math.abs(activeMonitor.x - bestMonitorCandidate.rect.x))
+                        bestMonitorCandidate = m;
+                }
+            }
+            break;
+        case SCHEMA_FOCUS_RIGHT:
+            for (let m of monitors) {
+                if (m.rect.x > activeMonitor.x) {
+                    if (!bestMonitorCandidate)
+                        bestMonitorCandidate = m;
+                    else if (Math.abs(activeMonitor.y - m.rect.y) < Math.abs(activeMonitor.y - bestMonitorCandidate.rect.y))
+                        bestMonitorCandidate = m;
+                }
+            }
+            break;
+        case SCHEMA_FOCUS_LEFT:
+            for (let m of monitors) {
+                if (m.rect.x < activeMonitor.x) {
+                    if (!bestMonitorCandidate)
+                        bestMonitorCandidate = m;
+                    else if (Math.abs(activeMonitor.y - m.rect.y) < Math.abs(activeMonitor.y - bestMonitorCandidate.rect.y))
+                        bestMonitorCandidate = m;
+                }
+            }
+            break;
+        }
+
+        if (bestMonitorCandidate)
+            return bestMonitorCandidate.id;
+
+
+        return null;
+    }
+
+    _getBestCandidate(id, monitor, activeRect) {
+        const windows = this._getAllWindows(monitor);
+        const { x, y } = activeRect;
+        let bestCandidate = null;
+        switch (id) {
+        case SCHEMA_FOCUS_UP:
+            windows.forEach(w => {
+                const rect = w.get_frame_rect();
+                if (rect.y < y) {
+                    if (!bestCandidate) {
+                        bestCandidate = w;
+                    } else {
+                        const bestRect = bestCandidate.get_frame_rect();
+                        if (Math.abs(activeRect.x - rect.x) < Math.abs(activeRect.x - bestRect.x))
+                            bestCandidate = w;
+                    }
+                }
+            });
+            break;
+        case SCHEMA_FOCUS_DOWN:
+            windows.forEach(w => {
+                const rect = w.get_frame_rect();
+                if (rect.y > y) {
+                    if (!bestCandidate) {
+                        bestCandidate = w;
+                    } else {
+                        const bestRect = bestCandidate.get_frame_rect();
+                        if (Math.abs(activeRect.x - rect.x) < Math.abs(activeRect.x - bestRect.x))
+                            bestCandidate = w;
+                    }
+                }
+            });
+            break;
+        case SCHEMA_FOCUS_RIGHT:
+            windows.forEach(w => {
+                const rect = w.get_frame_rect();
+                if (rect.x > x) {
+                    if (!bestCandidate) {
+                        bestCandidate = w;
+                    } else {
+                        const bestRect = bestCandidate.get_frame_rect();
+                        if (Math.abs(activeRect.y - rect.y) < Math.abs(activeRect.y - bestRect.y))
+                            bestCandidate = w;
+                    }
+                }
+            });
+            break;
+        case SCHEMA_FOCUS_LEFT:
+            windows.forEach(w => {
+                const rect = w.get_frame_rect();
+                if (rect.x < x) {
+                    if (!bestCandidate) {
+                        bestCandidate = w;
+                    } else {
+                        const bestRect = bestCandidate.get_frame_rect();
+                        if (Math.abs(activeRect.y - rect.y) < Math.abs(activeRect.y - bestRect.y))
+                            bestCandidate = w;
+                    }
+                }
+            });
+            break;
+        }
+
+        if (!bestCandidate) {
+            const newMonitor = this._getMonitorByDirection(id, monitor);
+            if (newMonitor !== null)
+                return this._getBestCandidate(id, newMonitor, this._activeWindow.get_display().get_monitor_geometry(monitor));
+        }
+
+        return bestCandidate;
+    }
+
+    _getAllWindows(monitor) {
+        const workspace = this._workspaceManager.get_active_workspace();
+        const windows = workspace.list_windows();
+        return windows.filter(w => w.get_monitor() === monitor);
+    }
+
+    _getActiveWindow() {
+        const workspace = this._workspaceManager.get_active_workspace();
+        const windows = workspace.list_windows();
+
+        let focusedWindow = null;
+        let focusedWindowRect = null;
+
+        for (let window of windows) {
+            if (window.has_focus()) {
+                focusedWindow = window;
+                focusedWindowRect = window.get_frame_rect();
+                break;
+            }
+        }
+
+        if (focusedWindow)
+            return { activeWindow: focusedWindow, activeRect: focusedWindowRect };
+        else
+            return null;
+    }
+
+    _bindShortcut() {
+        this._keyFocusUpId = SCHEMA_FOCUS_UP;
+        this._keyFocusDownId = SCHEMA_FOCUS_DOWN;
+        this._keyFocusRightId = SCHEMA_FOCUS_RIGHT;
+        this._keyFocusLeftId = SCHEMA_FOCUS_LEFT;
+
+        Main.wm.addKeybinding(
+            this._keyFocusUpId,
+            this._settings,
+            Meta.KeyBindingFlags.NONE,
+            Shell.ActionMode.ALL,
+            () => this.changeFocus(this._keyFocusUpId)
+        );
+        Main.wm.addKeybinding(
+            this._keyFocusDownId,
+            this._settings,
+            Meta.KeyBindingFlags.NONE,
+            Shell.ActionMode.ALL,
+            () => this.changeFocus(this._keyFocusDownId)
+        );
+        Main.wm.addKeybinding(
+            this._keyFocusRightId,
+            this._settings,
+            Meta.KeyBindingFlags.NONE,
+            Shell.ActionMode.ALL,
+            () => this.changeFocus(this._keyFocusRightId)
+        );
+        Main.wm.addKeybinding(
+            this._keyFocusLeftId,
+            this._settings,
+            Meta.KeyBindingFlags.NONE,
+            Shell.ActionMode.ALL,
+            () => this.changeFocus(this._keyFocusLeftId)
+        );
+    }
+
+    _unbindShortcut() {
+        if (this._keyFocusUpId !== null)
+            Main.wm.removeKeybinding(this._keyFocusUpId);
+        if (this._keyFocusDownId !== null)
+            Main.wm.removeKeybinding(this._keyFocusDownId);
+        if (this._keyFocusRightId !== null)
+            Main.wm.removeKeybinding(this._keyFocusRightId);
+        if (this._keyFocusLeftId !== null)
+            Main.wm.removeKeybinding(this._keyFocusLeftId);
+
+        this._keyFocusUpId = null;
+        this._keyFocusDownId = null;
+        this._keyFocusRightId = null;
+        this._keyFocusLeftId = null;
+    }
+
+    enable() {
+        log(`Enabling ${Me.metadata.name} version ${Me.metadata.version}`);
+        this._bindShortcut();
+    }
+
+    disable() {
+        log(`Disabling ${Me.metadata.name} version ${Me.metadata.version}`);
+        this._unbindShortcut();
+    }
+}
+
+// eslint-disable-next-line no-unused-vars
+function init() {
+    log(`Initializing ${Me.metadata.name} version ${Me.metadata.version}`);
+    return new FocusChanger();
+}
---
+++ prefs.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const Gtk = imports.gi.Gtk;
+const GObject = imports.gi.GObject;
+const ExtensionUtils = imports.misc.extensionUtils;
+
+const COLUMN_ID          = 0;
+const COLUMN_DESC = 1;
+const COLUMN_KEY         = 2;
+const COLUMN_MODS        = 3;
+
+// eslint-disable-next-line no-unused-vars
+function init() {
+}
+
+// eslint-disable-next-line no-unused-vars
+function buildPrefsWidget() {
+    const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.focus-changer');
+
+    const prefsWidget = new Gtk.Grid({
+        margin_top: 20,
+        margin_bottom: 20,
+        margin_start: 20,
+        margin_end: 20,
+        row_spacing: 20,
+    });
+
+    const keybindingLabel  = new Gtk.Label({
+        label: 'Keyboard shortcuts',
+        hexpand: true,
+        halign: Gtk.Align.START,
+    });
+    prefsWidget.attach(keybindingLabel, 0, 0, 1, 1);
+
+
+    // Setup the store
+    let store = new Gtk.ListStore();
+    store.set_column_types(
+        [GObject.TYPE_STRING, // COLUMN_ID
+            GObject.TYPE_STRING, // COLUMN_DESC
+            GObject.TYPE_INT,    // COLUMN_KEY
+            GObject.TYPE_INT]);  // COLUMN_MODS
+
+    addKeybinding(store, settings, 'focus-up', 'Focus up');
+    addKeybinding(store, settings, 'focus-down', 'Focus down');
+    addKeybinding(store, settings, 'focus-left', 'Focus left');
+    addKeybinding(store, settings, 'focus-right', 'Focus right');
+
+    let treeView = new Gtk.TreeView();
+    treeView.model = store;
+    treeView.headers_visible = false;
+
+    // Desc text
+    let  renderer, column;
+    renderer = new Gtk.CellRendererText();
+    column = new Gtk.TreeViewColumn();
+    column.expand = true;
+    column.pack_start(renderer, true);
+    column.add_attribute(renderer, 'text', COLUMN_DESC);
+    treeView.append_column(column);
+
+    // Key binding
+    renderer = new Gtk.CellRendererAccel();
+    renderer.accel_mode = Gtk.CellRendererAccelMode.GTK;
+    renderer.editable = true;
+    column = new Gtk.TreeViewColumn();
+    column.pack_end(renderer, false);
+    column.add_attribute(renderer, 'accel-key', COLUMN_KEY);
+    column.add_attribute(renderer, 'accel-mods', COLUMN_MODS);
+    treeView.append_column(column);
+    prefsWidget.attach(treeView, 0, 1, 2, 1);
+
+
+    // Events
+    renderer.connect('accel-edited',
+        (_, path, key, mods, __) => {
+            let [ok, iter] = store.get_iter_from_string(path);
+            if (!ok)
+                return;
+
+            store.set(iter, [COLUMN_KEY, COLUMN_MODS], [key, mods]);
+
+            let id = store.get_value(iter, COLUMN_ID);
+            let accelString = Gtk.accelerator_name(key, mods);
+            settings.set_strv(id, [accelString]);
+        });
+
+    renderer.connect('accel-cleared',
+        (_, path) => {
+            let [ok, iter] = store.get_iter_from_string(path);
+            if (!ok)
+                return;
+
+            store.set(iter, [COLUMN_KEY, COLUMN_MODS], [0, 0]);
+
+            let id = store.get_value(iter, COLUMN_ID);
+            settings.set_strv(id, []);
+        });
+
+    return prefsWidget;
+}
+
+
+function addKeybinding(model, settings, id, description) {
+    // Get the current accelerator.
+    let accelerator = settings.get_strv(id)[0];
+    let key, mods;
+    if (accelerator === null)
+        [key, mods] = [0, 0];
+    else
+        [, key, mods] = Gtk.accelerator_parse(settings.get_strv(id)[0]);
+
+    let row = model.insert(100);
+    model.set(row,
+        [COLUMN_ID, COLUMN_DESC, COLUMN_KEY, COLUMN_MODS],
+        [id,        description,        key,        mods]);
+}
